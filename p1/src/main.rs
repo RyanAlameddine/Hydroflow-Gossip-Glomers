e996f37de843e86ee20b311fd5a24386215c4571
Ryan Alameddine <rhalameddin@gmail.com>
2023-04-21 01:13:57 -0700

    cleaned up code

--- p1/src/main.rs
+++ p1/src/main.rs
@@ -1,16 +1,39 @@
 mod protocol;
 
 use hydroflow::{tokio, hydroflow_syntax, serde_json::{self, from_str}};
+use protocol::EchoMsg;
 
 use crate::protocol::{Message, Body, InitOkMsg, InitMsg, EchoOkMsg};
 //{"src": "c1", "dest": "n3", "body": {"type": "init", "msg_id": 1, "node_id": "n3", "node_ids": ["n1", "n2", "n3"]}}
 //./maelstrom/maelstrom test -w echo --bin ./target/debug/hydroflow-template.exe --time-limit 5
 
+fn create_init_ok((dest, body) : (String, InitMsg)) -> String {
+    let message = Message {
+        src: body.node_id, 
+        dest, 
+        body: Body::init_ok(InitOkMsg {in_reply_to: body.msg_id})
+    };
+    serde_json::to_string(&message).unwrap()
+}
+
+fn create_echo_ok((msg_id, (node_id, (dest, body))) : (i32, (String, (String, EchoMsg)))) -> String {
+    let message = Message {
+        src: node_id, 
+        dest, 
+        body: Body::echo_ok(EchoOkMsg {
+            msg_id: Some(msg_id),
+            in_reply_to: body.msg_id,
+            echo: body.echo
+        })
+    };
+    serde_json::to_string(&message).unwrap()
+}
+
 #[tokio::main]
 async fn main() {
 
     let mut flow = hydroflow_syntax! {
-        inbound_chan = source_stdin() -> map(Result::unwrap) -> map(|x| from_str(&x)) -> map(Result::unwrap);
+        inbound_chan = source_stdin() -> map(Result::unwrap) -> map(|x| from_str(&x).unwrap());
 
         channels = inbound_chan -> demux(|o : Message, var_args!(init, error, echo)|
             match o.body {
@@ -24,37 +47,21 @@ async fn main() {
 
         init = channels[init] -> tee();
         //Respond with InitOk
-        init[0] -> map(|(dest, body) : (String, InitMsg)| 
-            Message {
-                src: body.node_id, 
-                dest, 
-                body: Body::init_ok(InitOkMsg {in_reply_to: body.msg_id})
-            }) 
-            -> for_each(|x| println!("{}",  serde_json::to_string(&x).unwrap()));
+        init[0] -> map(create_init_ok) -> [0]out;
           
         //Save node id for later use
         node_id = init[1] -> map(|(_, body) : (_, InitMsg)| body.node_id);
         
+        //cross join node_id with echo channel
         id_echo = cross_join();
         node_id        -> [0]id_echo;
         channels[echo] -> [1]id_echo;
 
-        id_echo -> enumerate() -> map(|(msg_id, (node_id, (dest, body)))| 
-            Message {
-                src: node_id, 
-                dest, 
-                body: Body::echo_ok(EchoOkMsg {
-                    msg_id: Some(msg_id),
-                    in_reply_to: body.msg_id,
-                    echo: body.echo
-                })
-            }) 
-            -> for_each(|x| println!("{}",  serde_json::to_string(&x).unwrap()));
+        //for each echo, generate a response
+        id_echo -> enumerate() -> map(|x| create_echo_ok(x)) -> [1]out;
 
-        //out = channels[echo];// -> map(|(x, y)| );   
-        
-        //out -> for_each(|x| println!("Echo: {:?}", x));
 
+        out = merge() -> for_each(|x| println!("{}",  x));
     };
 
     flow.run_async().await;
